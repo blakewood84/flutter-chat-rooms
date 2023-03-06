e94c89a547ebd1cdd69c286b251250749e1ac975
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-03-06 15:09:15 -0800

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
 void main() async {
-  runApp(const MyApp());
+  final client = StreamChatClient(
+    '9dtqfpkev6vq',
+    logLevel: Level.INFO,
+  );
+
+  await client.connectUser(
+    User(
+      id: 'new-user3',
+      role: 'admin',
+      name: "Blake Man",
+      image: "https://i.imgur.com/fR9Jz14.png",
+    ),
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibmV3LXVzZXIzIn0.yLO8ARkaBtpAVteaHyU7W5xlN75EMsNhK95av8xasF4',
+  );
+
+  runApp(MyApp(
+    client: client,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({required this.client, super.key});
+
+  final StreamChatClient client;
 
   // This widget is the root of your application.
   @override
@@ -16,27 +35,33 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark(
         useMaterial3: true,
       ),
-      home: const ChatRoom(),
+      builder: (context, widget) {
+        return StreamChat(
+          client: client,
+          child: widget,
+        );
+      },
+      home: ChatRoom(
+        client: client,
+      ),
     );
   }
 }
 
 class ChatRoom extends StatefulWidget {
-  const ChatRoom({super.key});
+  const ChatRoom({
+    required this.client,
+    super.key,
+  });
+
+  final StreamChatClient client;
 
   @override
   State<ChatRoom> createState() => _ChatRoomState();
 }
 
 class _ChatRoomState extends State<ChatRoom> {
-  final apiKey = "9dtqfpkev6vq";
-  final client = StreamChatClient(
-    '9dtqfpkev6vq',
-    logLevel: Level.INFO,
-  );
-
   final _finishedLoadingNotifier = ValueNotifier(false);
-
   late final Channel channel;
 
   @override
@@ -46,16 +71,7 @@ class _ChatRoomState extends State<ChatRoom> {
   }
 
   void setAndConnectUser() async {
-    await client.connectUser(
-      User(
-        id: 'new-user2',
-        name: "Blake Man",
-        image: "https://i.imgur.com/fR9Jz14.png",
-      ),
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibmV3LXVzZXIyIn0.ex3zllYgTjY8Lc16_XtEmOx80ABRTJouYyo_So-Qf0I',
-    );
-
-    channel = client.channel('messaging', id: 'some-channel');
+    channel = widget.client.channel('messaging', id: 'some-channel');
 
     await channel.watch();
 
@@ -65,33 +81,30 @@ class _ChatRoomState extends State<ChatRoom> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-
+    // return const SizedBox.shrink();
     return Scaffold(
-      body: StreamChat(
-        client: client,
-        child: ValueListenableBuilder(
-          valueListenable: _finishedLoadingNotifier,
-          builder: (__, bool isFinished, _) {
-            if (!isFinished) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            return StreamChannel(
-              channel: channel,
-              child: Column(
-                children: const [
-                  StreamChannelHeader(),
-                  Expanded(
-                    child: StreamMessageListView(),
-                  ),
-                  StreamMessageInput(),
-                ],
-              ),
+      body: ValueListenableBuilder(
+        valueListenable: _finishedLoadingNotifier,
+        builder: (__, bool isFinished, _) {
+          if (!isFinished) {
+            return const Center(
+              child: CircularProgressIndicator(),
             );
-          },
-        ),
+          }
+
+          return StreamChannel(
+            channel: channel,
+            child: Column(
+              children: const [
+                StreamChannelHeader(),
+                Expanded(
+                  child: StreamMessageListView(),
+                ),
+                StreamMessageInput(),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
